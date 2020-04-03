02c3c1654f26d7e797a9721dc9b5b7a9e55d09fb
sidikhalifa <sidi.khalifa@live.fr>
2020-04-03 17:53:37 +0200

    add header

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import Layout from "../components/Layout";
 import Title from "../components/Title";
+import Header from "../components/Header";
 
 const Home = () => (
     <div className="container">
         <Head>
             <title>Easyblue: test frontend</title>
             <link rel="icon" href="/favicon.ico"/>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
         </Head>
 
         <Layout>
+            <Header />
             <Title text="Test frontend."/>
         </Layout>
     </div>
